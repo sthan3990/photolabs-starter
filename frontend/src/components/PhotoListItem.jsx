bd95317a6a7786166400e53f63d6094c2a110e4d
phankons <stephen.han@gmx.com>
2023-08-17 18:37:36 -0400

    Display 3 Different Photos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,28 +4,35 @@ import "../styles/PhotoListItem.scss";
 import "../styles/PhotoList.scss";
 
 const PhotoListItem = (props) => {
-  const photoData = props.PhotoListData;
 
-  return (
+  // Create array
+  const photos = props.PhotoListData;
 
+  const photoItem = photos.slice(0,3).map(photo =>
+  <li key={photo.id} >
     <div className="photo-list__item">
 
-      <img className="photo-list__image" src={photoData.imageSource} />
+      <img className="photo-list__image" src={photo.urls.full} />
 
       <div className="photo-list__user-details">
-          
-          <img className="photo-list__user-profile" src={photoData.profile} />
-          
-          <div className="photo-list__user-info">
-            <span>{photoData.username}</span>
-            <br />
-            <span className="photo-list__user-location">{`${photoData.location.city}, ${photoData.location.country}`}</span>
-          </div>
-    
+
+        <img className="photo-list__user-profile" src={photo.user.profile} />
+
+        <div className="photo-list__user-info">
+          <span>{photo.username}</span>
+          <br />
+          <span className="photo-list__user-location">{`${photo.location.city}, ${photo.location.country}`}</span>
+        </div>
+
       </div>
-
     </div>
+    </li>
+  );
 
+  return (
+    <ul>
+      {photoItem}
+    </ul>
   );
 };
 
