776918ece29ae9403d3fb5735286220e6c2354ee
phankons <stephen.han@gmx.com>
2023-08-26 12:48:51 -0400

    Connect the Favourite Icon and Modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,17 @@ import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 const App = (props) => {
-  
+
   const [showModal, setShowModal] = useState(false);
   const [photoData, setPhotoData] = useState(null);
   const [selected, setSelected] = useState([]);
+  const [alert, setAlert] = useState(false);
 
   return (
     <div className="App">
       <HomeRoute
+        alert={alert}
+        setAlert={setAlert}
         setShowModal={setShowModal}
         setPhotoData={setPhotoData}
         setSelected={setSelected}
@@ -21,7 +24,7 @@ const App = (props) => {
       />
       {
         showModal ? <PhotoDetailsModal setSelected={setSelected}
-        selected={selected} photoData={photoData} toggleModal={setShowModal} /> : null
+          selected={selected} photoData={photoData} toggleModal={setShowModal} /> : null
       }
     </div>
   );
